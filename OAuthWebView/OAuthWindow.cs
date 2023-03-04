e83a718b004a76bf1d0b340452aae8bd080dd7bd
Elorucov <elorucov@live.ru>
2023-03-04 21:19:42 +0400

    Yet another changes for testing...

--- OAuthWebView/OAuthWindow.cs
+++ OAuthWebView/OAuthWindow.cs
@@ -18,51 +18,44 @@ namespace OAuthWebView {
             size = new Size(width, height);
         }
 
-        static bool initialized = false;
-        static Task task = new Task(() => Application.Run());
-
-        public static void Initialize() {
-            if (initialized) return;
-            initialized = true;
-
-
-#if WIN
-            SpiderEye.Windows.WindowsApplication.Init();
-#elif LINUX
-            SpiderEye.Linux.LinuxApplication.Init();
-#elif MAC
-            SpiderEye.Mac.MacApplication.Init();
-#endif
-
-            task.Start();
-        }
-
-        public static void Unititialize() {
-            if (!initialized) return;
-            initialized = false;
-
-            task.Dispose();
-        }
+        static bool appInitialized = false;
 
         public async Task<Uri> StartAuthenticationAsync() {
+            if (!appInitialized) {
+#if WIN
+                SpiderEye.Windows.WindowsApplication.Init();
+#elif LINUX
+                SpiderEye.Linux.LinuxApplication.Init();
+#elif MAC
+                SpiderEye.Mac.MacApplication.Init();
+#endif
+
+                appInitialized = true;
+            }
+
             mres = new ManualResetEventSlim();
-            using (window = new Window() {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine($"Current thread id: {thread.ManagedThreadId}");
+            System.Diagnostics.Debug.WriteLine($"Current thread id: {thread.ManagedThreadId}");
+
+            window = new Window() {
 #if LINUX
                 CanResize = true,
 #else
                 CanResize = false,
 #endif
                 Title = title,
-                Size = size
-            }) {
-                window.Navigating += Window_Navigating;
-                window.Closed += Window_Closed;
-                window.Show();
-                window.LoadUrl(startUri.ToString());
-                await Task.Factory.StartNew(() => {
-                    mres.Wait();
-                }).ConfigureAwait(true);
-            }
+                Size = size,
+                MinSize = size,
+                MaxSize = size
+            };
+            window.Navigating += Window_Navigating;
+            window.Closed += Window_Closed;
+            window.Show();
+            window.LoadUrl(startUri.ToString());
+            await Task.Factory.StartNew(() => mres.Wait()).ConfigureAwait(true);
+
+            // Cannot dispose the window object yet...
 
             mres.Dispose();
             return currentUri.AbsolutePath == endUri.AbsolutePath ? currentUri : null;
--- OAuthWebViewDemo/App.axaml.cs
+++ OAuthWebViewDemo/App.axaml.cs
@@ -15,7 +15,6 @@ namespace OAuthWebViewDemo {
             }
 
             base.OnFrameworkInitializationCompleted();
-            OAuthWindow.Initialize();
         }
     }
 }
